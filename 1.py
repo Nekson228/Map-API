e5f98a40ec9ed3dcea9996645c8331cf4ad2f629
Yandex Liceum 1 <bangerwockeez@gmail.com>
2021-02-09 16:44:16 +0500

    created interface

--- 1.py
+++ 1.py
@@ -4,15 +4,22 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main1.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        uic.loadUi('main.ui', self)
+        self.search_button.clicked.connect(self.search)
+
+    def search(self):
+        coords = self.coords_input.text()
+        zoom = self.zoom_slider.value()
+        print(coords, zoom)
+        # вызов функции вани
+        # вставка в лабел
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = Window()
     ex.show()
     sys.exit(app.exec_())
